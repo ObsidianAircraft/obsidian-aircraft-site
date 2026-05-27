aac491fdede0c7347cafb56afd359b0740db5aa7
JJ Harris <jjharris367@gmail.com>
2026-05-27 00:00:53 -0700

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import Aircraft from "./pages/Aircraft";
@@ -22,6 +23,8 @@ function App() {
         <Route path="/faq" element={<FAQ />} />
         <Route path="/join" element={<Join />} />
       </Routes>
+      
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+
+        <div className="footer-brand">
+          <h3>Obsidian Aircraft</h3>
+
+          <p>
+            Future-Proof Composite Aircraft Architecture
+          </p>
+        </div>
+
+        <nav className="footer-links">
+          <NavLink to="/aircraft">Aircraft</NavLink>
+          <NavLink to="/development">Development</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/faq">FAQ</NavLink>
+        </nav>
+
+        <div className="footer-bottom">
+          <p>© 2026 Obsidian Aircraft LLC</p>
+          <p>Vancouver, Washington</p>
+        </div>
+
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
