066b67007c149dea8c8551ad62b1303f5e7dfd0d
JJ Harris <jjharris367@gmail.com>
2026-06-03 21:04:20 -0700

    Initialized Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-L8NV8BG3B3");
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
